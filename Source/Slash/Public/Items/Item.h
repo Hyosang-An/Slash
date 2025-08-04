c5f199ffa226890eb0880972b119caf7ad62acfa
Hyosang_An <gytharj@gmail.com>
2025-08-04 15:05:58 +0900

    🌟 Update DefaultEngine.ini and enhance Item actor functionality
    - Changed `EditorStartupMap` to `Minimal_Default` for better initialization.
    - Enabled virtual textures in engine settings.
    - Added movement and rotation logic to `AItem` class in Tick function.
    - Introduced debug drawing macros for better visualization during development.

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -15,8 +15,8 @@ public:
 	// Sets default values for this actor's properties
 	AItem();
 	virtual void Tick(float DeltaTime) override;
+
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
-	
 };
