877c4b95b8530ec8b53177448c535110e7378d24
Hyosang_An <gytharj@gmail.com>
2025-08-07 00:55:19 +0900

    🎉 Add weapon functionality with overlap events and create BP_Weapon asset

--- Source/Slash/Public/Items/Item.h
+++ Source/Slash/Public/Items/Item.h
@@ -35,8 +35,11 @@ protected:
 	T Avg(T First, T Second);
 
 	UFUNCTION()
-	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
+	UFUNCTION()
+	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+	
 private:
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = "true"))
 	float RunningTime;
